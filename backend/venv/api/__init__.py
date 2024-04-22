e39d5c0edcbfad76ab706b136782e9963d7ce43c
saminchowdhury <sc45@njit.edu>
2024-04-21 21:27:15 -0400

    Init

--- backend/venv/api/__init__.py
+++ backend/venv/api/__init__.py
@@ -2,3 +2,4 @@ from . import getLanguageController
 from . import signupController
 from . import loginController
 from . import subscriptionLevelController
+from . import createTranslationController
